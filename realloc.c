f0082fab465f0a5ecb4deda39ca3a7aadc4b8a77
ahmednaser111 <ahmednaser7707@gmail.com>
2024-02-29 17:35:43 +0200

    getLine

--- realloc.c
+++ realloc.c
@@ -3,16 +3,16 @@
 /**
  **_memset - fills memory with a constant byte
  *@s: the pointer to the memory area
- *@b: the byte to fill *s with
+ *@ch: the byte to fill *s with
  *@n: the amount of bytes to be filled
  *Return: (s) a pointer to the memory area s
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char ch, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+		s[i] = ch;
 	return (s);
 }
 
